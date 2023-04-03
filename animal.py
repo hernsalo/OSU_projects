c13e6241f831cdd23aca4590685e688c5683507f
hernsalo <hernsalo@oregonstate.edu>
2023-04-03 12:17:01 -0700

    Project 1

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Salomon Hernandez
+# GitHub username: hernsalo
+# Date: 4/3/2023
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
